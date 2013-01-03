b769fd4ae6ba89ec51f091f047f855d54ec24ef7
Ali Mihandoost <i@ali.md>
2013-01-04 02:41:13 +0330

    Add page 2 with animate

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 	$(window).bind('load',function(){setTimeout(function(){
 			window.scrollTo(0,0)
 			$(document.body).removeClass('preload')
-	},1000)});
+	},1)});
 
 	scrollorama.onBlockChange(function() {
 		var i = scrollorama.blockIndex
@@ -17,10 +17,50 @@
 		duration:250
 	},{
 		property:'opacity',
+		start:1,
 		end:0,
 		duration:250
 	})
 
-	//.animate();
+	.animate('.p2 .tx1',{
+		property:'margin-top',
+		start:100,
+		end:200,
+		duration:200,
+		pin:true
+	},{
+		property:'opacity',
+		start:0,
+		end:1,
+		duration:200
+	})
+
+	.animate('.p2 .tx2',{
+		property:'margin-top',
+		start:10,
+		end:40,
+		duration:150,
+		delay:300,
+		pin:true
+	},{
+		property:'opacity',
+		start:0,
+		end:1,
+		duration:150,
+		delay:300
+	})
+
+	.animate('.p2 .tx1',{
+		property:'margin-top',
+		start:200,
+		end:210,
+		duration:200,
+		delay:450,
+		pin:true
+	})
+
+	
+	
+
 
 })(jQuery)
