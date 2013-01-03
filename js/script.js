1b5870cc1f4cbb04f908dfbd84338d0fe4a55dd4
Ali Mihandoost <i@ali.md>
2013-01-04 00:51:18 +0330

    Init scroll

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-var scrollorama = $.scrollorama({ blocks:'.pages' });
+;(function($,undefined){
+	var scrollorama = $.scrollorama({ blocks:'.pages' })
 
-scrollorama.onBlockChange(function() {
-	var i = scrollorama.blockIndex;
-	$('.pages').removeClass('active').eq(i).addClass('active');
-});
+	$(window).bind('load',function(){setTimeout(function(){
+			window.scrollTo(0,0)
+
+			$(document.body).removeClass('preload')
+
+			scrollorama.onBlockChange(function() {
+				var i = scrollorama.blockIndex
+				$('.pages').removeClass('active').eq(i).addClass('active')
+			});
+
+	},1000)});
+})(jQuery)
