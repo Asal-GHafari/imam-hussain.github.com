cf0540f37253707551da75fd052fac041ee6c830
Ali Mihandoost <i@ali.md>
2013-01-04 14:33:36 +0330

    P5 Animate

--- js/script.js
+++ js/script.js
@@ -14,12 +14,16 @@
 		bookimg.css('display', i<3 || i>10?'none':'block')
 	});
 
+	// p1
+
 	scrollorama.animate('.p1 .tx1',{
 		property:'opacity',
 		start:1,
 		end:0,
 		duration:200
 	})
+
+	// p2
 
 	var dly = 300
 
@@ -39,7 +43,7 @@
 			duration:150,
 			delay:dly
 		})
-		dly += 100
+		dly += 150
 	})
 
 	scrollorama.animate('.p2 .group',{
@@ -50,6 +54,8 @@
 		delay:dly+50
 	})
 
+
+	// p3
 
 	dly = 300
 
@@ -69,7 +75,7 @@
 			duration:150,
 			delay:dly
 		})
-		dly += 100
+		dly += 150
 	})
 
 	scrollorama.animate('.p3 .group',{
@@ -80,15 +86,46 @@
 		delay:dly+50
 	})
 
+	// p4
+
+	dly = 200
+
+	scrollorama.animate('.p4 .tx1',{
+		property:'margin-top',
+		start:0,
+		end:300,
+		duration:500,
+		delay: dly
+	})
+
+	$('.p4 h1, .p4 h2, .p4 h3').each(function(){
+		scrollorama.animate($(this),{
+			property:'opacity',
+			start:0,
+			end:1,
+			duration:150,
+			delay:dly
+		})
+		dly += 150
+	})
+
+	scrollorama.animate('.p4 .group',{
+		property:'opacity',
+		start:1,
+		end:0,
+		duration:250,
+		delay:dly+50
+	})
+
+	// p5
 
 	scrollorama.animate(bookimg,{
 		property:'opacity',
 		start:0,
 		end:1,
 		duration:150,
-		delay:300
+		delay:600
 	})
-
 
 
 
